79e9e84f4655414a6a2f66665820c89581a09142
ayinfly <ayinflyschool@gmail.com>
2023-12-03 16:50:27 -0800

    turned previous stuff into react idk

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+import React, { useState } from 'react';
+import ChatForm from './gpt-forms/ChatForm';
+import ChatResponse from './gpt-forms/ChatResponse';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [response, setResponse] = useState('');
+
+  const handleSendMessage = async (userInput) => {
+    const response = await fetch('/get_response', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ user_input: userInput })
+    });
+    setResponse(await response.text());
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Chat with GPT</h1>
+      <ChatForm onSendMessage={handleSendMessage} />
+      <ChatResponse response={response} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/gpt-forms/ChatResponse.jsx
@@ -0,0 +1,13 @@
+// ChatResponse.js
+import React from 'react';
+
+function ChatResponse({ response }) {
+  return (
+    <>
+      <h2>Response:</h2>
+      <p>{response}</p>
+    </>
+  );
+}
+
+export default ChatResponse;
