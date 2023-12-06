320efbcfd7eaafe86bcc1d5c92f72e730dc9d7cb
ayinfly <ayinflyschool@gmail.com>
2023-12-05 17:56:03 -0800

    sample popup

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,9 +2,12 @@
 import React, { useState } from 'react';
 import ChatForm from './gpt-forms/ChatForm';
 import ChatResponse from './gpt-forms/ChatResponse';
+import Popup from './components/Popup'; // Make sure to import the Popup component
+
 
 function App() {
   const [response, setResponse] = useState('');
+  const [showPopup, setShowPopup] = useState(true);
 
   const handleSendMessage = async (userInput) => {
     const response = await fetch('/get_response', {
@@ -18,11 +21,16 @@ function App() {
     setResponse(await response.text());
   };
 
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
     <div>
       <h1>Chat with GPT</h1>
       <ChatForm onSendMessage={handleSendMessage} />
       <ChatResponse response={response} />
+      {showPopup && <Popup onClose={handleClosePopup} />}
     </div>
   );
 }
